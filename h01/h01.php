424df33695c92b28c108d43e5daad52cb9fab7e5
justin <jvanderheide@roc-dev.com>
2020-09-25 08:55:11 +0200

    einde opdracht h01

--- h01/h01.php
+++ h01/h01.php
@@ -4,23 +4,23 @@ $a = "ik heb niet gefaalt";
 $b = "die niet werken";
 $c = "10000";
 
-echo $a." ik heb alleen ".$c." manieren gevonden ".$b;
+echo $a." ik heb alleen ".$c." manieren gevonden ".$b."<br>";
 
 $d = "die geen fout";
 $e = "en";
 $f = "niet.";
 
 
-echo "mensen ".$d."".$e." maken, werken ".$f;
+echo "mensen ".$d."".$e." maken, werken ".$f."<br>";
 
 $g = "ry";
 $h = "but";
 $i = "fail";
 
-echo "T".$g." and ".$i.", ".$h." never fail to try.";
+echo "T".$g." and ".$i.", ".$h." never fail to try."."<br>";
 
 $j ="\"omgaan ";
 $k = "met ";
 $l = "teleu";
 
-echo "De cursus ".$j.$k.$l."rstellingen "."kan vanavond helaas niet doorgaan.";
+echo "De cursus ".$j.$k.$l."rstellingen\" "."kan vanavond helaas niet doorgaan."."<br>";
