6e7e3be9654fa556761440a4223a9edee0832776
justin <jvanderheide@roc-dev.com>
2020-09-25 02:54:42 +0200

    Initial commit

---
+++ h01/h01.php
@@ -0,0 +1,26 @@
+<?php
+
+$a = "ik heb niet gefaalt";
+$b = "die niet werken";
+$c = "10000";
+
+echo $a." ik heb alleen ".$c." manieren gevonden ".$b;
+
+$d = "die geen fout";
+$e = "en";
+$f = "niet.";
+
+
+echo "mensen ".$d."".$e." maken, werken ".$f;
+
+$g = "ry";
+$h = "but";
+$i = "fail";
+
+echo "T".$g." and ".$i.", ".$h." never fail to try.";
+
+$j ="\"omgaan ";
+$k = "met ";
+$l = "teleu";
+
+echo "De cursus ".$j.$k.$l."rstellingen "."kan vanavond helaas niet doorgaan.";
